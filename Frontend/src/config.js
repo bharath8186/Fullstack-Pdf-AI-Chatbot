88750ad8e385b2ac9feff0d90a7071c5221575c4
bharath8186 <bharathbk0808@gmail.com>
2026-05-24 21:55:40 +0530

    fix backend url

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://ai-pdf-chatbot-ljzd.onrender.com";
+const API_BASE_URL = "https://fullstack-pdf-ai-chatbot.onrender.com";
 
 export default API_BASE_URL;
