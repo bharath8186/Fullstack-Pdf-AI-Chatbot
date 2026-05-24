c0df9b85a075bea5950d617e8dbd997f1e902d2a
bharath8186 <bharathbk0808@gmail.com>
2026-05-24 17:30:38 +0530

    fix CORS

--- Backend/main.py
+++ Backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="RAG PDF Chatbot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins
+    allow_origins=["https://ai-pdf-chatbot-ljzd.onrender.com"],  # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
